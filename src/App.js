657d0a32feb8ce3bba25daea6fbe16b7d4d4c515
Vinicius José <vinicius.jsilv@gmail.com>
2021-08-06 19:10:45 -0300

    Implementação dos estilos da baixa e header

--- src/App.js
+++ src/App.js
@@ -1,29 +1,71 @@
 import React, { useEffect, useState } from "react";
 import Tmdb from "./Tmdb";
+import './App.css';
+import Header from "./components/Header";
+import FeaturedMovie from "./components/FeaturedMovie";
 import MovieRow from "./components/MovieRow";
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(()=>{
     const loadAll = async () => {
+
+      //Requisitando todos os filmes
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      //Requisitando Filme em destaque
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
     }
 
     loadAll();
   }, [])
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true);
+      }else{
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, []);
+
   return (
     <div className="page">
+     <Header black={blackHeader}/>
+     {featuredData &&
+        <FeaturedMovie item={featuredData}/>
+     }
+     
       <section className="lists">
         {movieList.map((item, key) => (
           <div>
            <MovieRow key={key} title={item.title} items={item.items}/>
           </div>
-        ))};
+        ))}
       </section>
+
+      <footer>
+        <p>Desenvolvido por <a href="https://www.linkedin.com/in/vinicius-jos%C3%A9-silva-408510156/" target="_blank">Vinícius José Silva</a></p>
+        <p>Todos os direitos Reservados para <a href="https://www.netflix.com/br/" target="_blank">Netflix</a></p>
+        <p>Dados utilizados do site <a href="https://www.themoviedb.org/?language=pt-BR" target="_blank">The Movie DB</a></p>
+        <p>2021</p>
+      </footer>
     </div>
   );
 };
